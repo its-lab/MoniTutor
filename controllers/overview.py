2fb3738055fa8c9786f83e752c0566b66f36e284
Mark Swillus <mark.swillus@alumni.fh-aachen.de>
2017-01-20 16:36:56 +0100

    Display last actions of each user on slides

--- controllers/overview.py
+++ controllers/overview.py
@@ -44,24 +44,31 @@ def reveal_progress():
     if len(request.args) is 0:
         redirect(URL('default','index'))
     scenario_id = request.args(0, cast=int)
+    amount = 5
+    only_successful = 1
+    if len(request.args) > 1:
+        amount = request.args(1, cast=int)
+    if len(request.args) > 2:
+        only_successful = 0
+
     if auth.has_membership("admin"):
         update_progress(scenario_id)
     user_scenairo = tutordb((tutordb.scenario_user.scenario_id == scenario_id) &
                             (tutordb.scenario_user.user_id == tutordb.auth_user.id) &
                             (tutordb.scenario_user.status == "initiated")
                             ).select(orderby=tutordb.scenario_user.progress)
-    return dict(user_scenario=user_scenairo, scenario_id=scenario_id)
+    reveal_autoscroll = 15
+    return dict(user_scenario=user_scenairo, scenario_id=scenario_id, amount=amount, only_successful=only_successful, reveal_autoscroll=reveal_autoscroll)
 
 
 @auth.requires_membership("admin")
 def get_user_events():
-    scenario_id = request.args(0, cast=int)
-    user_id = request.args(1, cast=int)
-    amount = request.args(2, cast=int)
-    if len(request.args) > 3:
-        only_successful = True
-    else:
-        only_successful = False
+    scenario_id = request.vars.scenarioId
+    user_id = request.vars.userId
+    amount = 5
+    only_successful = request.vars.onlySuccessful
+    if request.vars.amount != amount:
+        amount = int(request.vars.amount)
     
     check_rows = tutordb((tutordb.monitutor_milestone_scenario.scenario_id == scenario_id) &
                          (tutordb.monitutor_milestones.milestone_id == 
@@ -78,15 +85,17 @@ def get_user_events():
         check_name = username +"_"+ check.monitutor_checks.name
         checks_by_name[check_name] = check
     
-    if only_successful:
+    if only_successful is 0:
         service_hist = db((db.icinga_statehistory.object_id ==   db.icinga_objects.object_id) &
                       (db.icinga_objects.is_active == 1) &
                       (db.icinga_statehistory.state == 0) &
+                      (db.icinga_statehistory.state != db.icinga_statehistory.last_state) &
                       (db.icinga_objects.name2.startswith(str(username)))).select(orderby=~db.icinga_statehistory.state_time,
                               limitby=(0,amount))
     else:
         service_hist = db((db.icinga_statehistory.object_id ==   db.icinga_objects.object_id) &
                       (db.icinga_objects.is_active == 1) &
+                      (db.icinga_statehistory.state != db.icinga_statehistory.last_state) &
                       (db.icinga_objects.name2.startswith(str(username)))).select(orderby=~db.icinga_statehistory.state_time,
                               limitby=(0,amount))
 
@@ -95,16 +104,16 @@ def get_user_events():
         if event.icinga_objects.name2 in checks_by_name:
             check_info = checks_by_name[event.icinga_objects.name2]
             check_events.append({
-                "check-info": 
+                "checkInfo": 
                 {
                     "hint": check_info.monitutor_checks.hint,
                     "displayName": check_info.monitutor_checks.display_name,
                 },
-                "check-state":
+                "checkState":
                 {
                     "state": event.icinga_statehistory.state,
                     "lastState": event.icinga_statehistory.last_state,
-                    "time": str(event.icinga_statehistory.state_time),
+                    "time": str((datetime.datetime.now(event.icinga_statehistory.state_time.tzinfo)-event.icinga_statehistory.state_time).total_seconds()).split('.',2)[0],
                     "output": event.icinga_statehistory.output
                 }
             })
