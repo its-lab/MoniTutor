be8485a95582443e31d38415c70ebbbc238d8167
Mark Swillus <mark.swillus@alumni.fh-aachen.de>
2017-09-08 14:24:57 +0200

    Rename test vars

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -13,6 +13,15 @@ class NewAdminTest(unittest.TestCase):
         self.profile.assume_untrusted_cert_issuer = True
         self.browser = webdriver.Firefox(firefox_profile=self.profile,
                                          capabilities={"marionette": False})
+        self.testuser = { "name": "Monty",
+                          "surname": "Python",
+                          "email": "monty.python@example.net",
+                          "username": "administrator",
+                          "password": "securepassowrd"}
+        self.hostname = "localhost"
+        self.admin_password = "admin"
+        self.path_to_scenario_file = "~/monitutor_scenarios/example.json"
+
 
     def tearDown(self):
         self.browser.quit()
@@ -23,7 +32,7 @@ class NewAdminTest(unittest.TestCase):
     def test_user_can_sign_up(self):
         # The MoniTutor stack was just set up. In order to get everything
         # prepared for the students, the admin first needs to register.
-        self.browser.get("https://localhost")
+        self.browser.get("https://"+self.hostname)
 
         # The Admin is redirected to the Login page.
         self.assertIn(u"Log In",
@@ -45,17 +54,17 @@ class NewAdminTest(unittest.TestCase):
 
         # The admin fills out the registration form
         self.browser.find_element_by_id("auth_user_first_name") \
-            .send_keys("Monty",
+            .send_keys(self.testuser["name"],
                        Keys.TAB,
-                       "Python",
+                       self.testuser["surname"],
                        Keys.TAB,
-                       "montypython@example.com",
+                       self.testuser["email"],
                        Keys.TAB,
-                       "administrator",
+                       self.testuser["username"],
                        Keys.TAB,
-                       "securepassword",
+                       self.testuser["password"],
                        Keys.TAB,
-                       "securepassword",
+                       self.testuser["password"],
                        Keys.ENTER)
         self.wait_for_page_to_load()
         self.assertRaises(NoSuchElementException,
@@ -64,14 +73,14 @@ class NewAdminTest(unittest.TestCase):
         # The Admin is redirected to the welcome page and is now logged in.
         # As noone assigned admin privileges to the admin yet, he can not
         # acces the admin area
-        self.assertIn(u"Welcome, Monty!",
+        self.assertIn(u"Welcome, "+self.testuser["name"]+"!",
                       self.browser.find_element_by_tag_name('h1').text,
                       "Welcome greeting not found")
         self.assertRaises(NoSuchElementException,
                           self.browser.find_element_by_link_text, "Admin")
 
         # After reading the Welcome message, the admin wants to sign out again.
-        self.browser.find_element_by_link_text(u"Welcome, Monty").click()
+        self.browser.find_element_by_link_text(u"Welcome, "+self.testuser["name"]).click()
         self.browser.find_element_by_partial_link_text(u"Logout").click()
         self.wait_for_page_to_load()
         self.assertIn(u"Log In",
@@ -81,20 +90,20 @@ class NewAdminTest(unittest.TestCase):
         # To gain admin privileges, the admin navigates to the web2py database
         # backend, enters the admin password and adds his user to the admin
         # group
-        self.browser.get("https://localhost/admin")
+        self.browser.get("https://"+self.hostname+"/admin")
         self.wait_for_page_to_load()
         self.browser.find_element_by_id("password") \
-            .send_keys("admin", Keys.ENTER)
+            .send_keys(self.admin_password, Keys.ENTER)
         self.wait_for_page_to_load()
         self.browser \
-            .get("https://localhost/MoniTutor/appadmin/insert/tutordb/auth_membership")
+            .get("https://"+self.hostname+"/MoniTutor/appadmin/insert/tutordb/auth_membership")
         self.wait_for_page_to_load()
         options = self.browser.find_elements_by_tag_name("option")
         self.assertIn([u"admin"],
                       [option.text.split()[:1] for option in options],
                       "Group 'admin' does not exist")
         self.browser.find_element_by_id("auth_membership_user_id") \
-            .send_keys("a", Keys.TAB, "a", Keys.TAB, Keys.ENTER)
+            .send_keys(self.testuser["username"], Keys.TAB, "admin", Keys.TAB, Keys.ENTER)
         self.wait_for_page_to_load()
         self.assertIn("new record inserted",
                       self.browser.find_element_by_class_name("alert-dismissable").text,
